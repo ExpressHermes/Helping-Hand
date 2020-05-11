c2991f494ed754c2b2f01ea7fa11d29e330e9c5b
Ashutosh Kumar <46455005+ashutoshkumar6@users.noreply.github.com>
2020-05-11 22:07:04 +0530

    Updating Views of MainApp

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from mainapp.models import helping_hand
 # Create your views here.
 
 def home_page(request):
@@ -8,8 +8,11 @@ def home_page(request):
 
 def map(request):
     if request.method == 'POST':
-        # TODO [INCOMPLETE]
         lon = request.POST.get('lon')
         lat = request.POST.get('lat')
         print(f'lon: {lon}, lat: {lat}')
+        new_req=helping_hand(event_name=request.POST['eventOrganizer'],date=request.POST['eventDate'],
+        place_name=request.POST['placeName'],longt=request.POST['lon'],lati=request.POST['lat'])
+        new_req.save()
+        return redirect('mainapp:map')
     return render(request, 'mainapp/map.html')
