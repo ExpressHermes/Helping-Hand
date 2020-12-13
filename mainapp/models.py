9119d04ff922fb93894b71d80d0280107459b5d9
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2020-12-13 16:48:26 +0530

    modified models.py, views.py, home.html of mainapp and deleted pycache and previous migration files. Also created a fresh db

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Event(models.Model):
     event_organizer = models.CharField(max_length=100)
@@ -9,3 +10,4 @@ class Event(models.Model):
     lon = models.DecimalField(max_digits=9,decimal_places=6)
     lat = models.DecimalField(max_digits=9,decimal_places=6)
     description = models.TextField(max_length=500, null=True, blank=True)
+    interested = models.ManyToManyField(User, related_name='interested', blank=True)
