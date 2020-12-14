2689189cf6b0f3ed4175f1f3961aa5ce6775067c
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2020-12-15 00:19:17 +0530

    Successfully added dashboard but some UI changes are required

--- register/models.py
+++ register/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    fullname = models.CharField(max_length=100);
+    fullname = models.CharField(max_length=100, blank=True);
 
     def save(self, *args, **kwargs):
-        self.fullname = self.first_name + ' ' + self.last_name
+        if self.fullname.strip() == '':
+            self.fullname = self.first_name + ' ' + self.last_name
+        if self.fullname.strip() == '':
+            self.fullname = self.username
         super(CustomUser, self).save(*args, **kwargs)
