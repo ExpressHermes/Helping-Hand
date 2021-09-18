bb5cdfbd8ab76f6ac3f3e79eea02844177c80723
Shivam Yadav <9876shivamyadav@gmail.com>
2021-09-18 18:09:48 +0530

    heroku deploy error fix

--- HelpingHand/settings.py
+++ HelpingHand/settings.py
@@ -1,5 +1,5 @@
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -117,4 +117,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 LOGIN_URL = "/register/login/"
 
 # Activate Django-Heroku.
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
