15a8d52a471ffc38eb7f1304e3d1392cfdfd5dbc
JessicaJohnBritto <jessjohnbritto@gmail.com>
2021-12-20 02:56:26 +0530

    Minor changes

--- HelpingHand/settings.py
+++ HelpingHand/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'HelpingHand.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_new.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
