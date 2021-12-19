fb4af894a8f08da64018c7dff4b391e4fdd6af2d
JessicaJohnBritto <jessjohnbritto@gmail.com>
2021-12-20 02:38:47 +0530

    Fixed issues in Reset Password feature

--- register/urls.py
+++ register/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('logout/', views.user_logout, name="logout"),
     path('activate/<uidb64>/<token>/', views.user_verify, name="activate"),  
     path('ForgotPassword/', views.forgot_password, name="forgot_password"), 
-    path('ResetPassword/<uidb64>/<token>/', views.reset_password, name="reset_password"),  
-    path('ResetPasswordDone/', views.reset_password_done, name="reset_password_done"),  
+    # path('ResetPassword/<uidb64>/<token>/', views.reset_password, name="reset_password"),  
+    # path('ResetPasswordDone/', views.reset_password_done, name="reset_password_done"),  
  
 ]
