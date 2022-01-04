b0b5ffbb8f322309c31ad7407b627ef14e7670cb
JessicaJohnBritto <jessjohnbritto@gmail.com>
2022-01-04 19:34:03 +0530

    SortEvents

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,4 +6,9 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('create_event/', views.create_event, name='create_event'),
+    path('food', views.food, name='food'),
+    path('medical', views.medical, name='medical'),
+    path('others', views.others, name='others'),
+    path('clothes', views.clothes, name='clothes'),
+
 ]
